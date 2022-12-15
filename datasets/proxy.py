b7c3bf9553f01cd3bb8f7d152b66519d65dee46a
Vlad Hondru <vlad.hondru25@gmail.com>
2022-12-15 21:44:49 +0200

    Updated cosine_similarity method

--- datasets/proxy.py
+++ datasets/proxy.py
@@ -5,25 +5,32 @@ from torchvision.io import read_image
 
 # Define dataset class
 class ProxyDataset(Dataset):
-    def __init__(self, images, labels, transform=None):
+    def __init__(self, images, labels, transform=None, return_path=False, soft_label=None):
         self.images = images
         self.labels = labels
+        self.soft_label = soft_label
         
         self.transform = transform
-
-        gen_dataset_path = 'generated_dataset'
-        self.proxy_imgs_path = os.path.join(gen_dataset_path,'images')
+        
+        self.return_path = return_path
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        image = read_image(f'{self.proxy_imgs_path}/{self.images[idx]}')
+        image = read_image(self.images[idx])
         image = image / 255.0
         
         label = self.labels[idx]
         if self.transform:
             image = self.transform(image)
+            
+        if self.soft_label:
+            soft_label = self.soft_label[idx]
+        else:
+            soft_label = None
+            
+        if self.return_path:
+            return image, label, self.images[idx], soft_label
         
-        return image, label
-    
+        return image, label, self.soft_label[idx]
